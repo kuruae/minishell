1016cc14849719b0cf873bbfc90617341e929b07
jacobscuzzi <j.baumfalk@yahoo.de>
2024-11-29 18:59:23 +0100

    evening push Nov 29

--- includes/get_signal.h
+++ includes/get_signal.h
@@ -6,18 +6,20 @@
 /*   By: jbaumfal <jbaumfal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 14:55:12 by jbaumfal          #+#    #+#             */
-/*   Updated: 2024/11/29 16:29:53 by jbaumfal         ###   ########.fr       */
+/*   Updated: 2024/11/29 18:53:40 by jbaumfal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_SIGNAL_H
 # define GET_SIGNAL_H
+# include <features.h>
+# define _POSIX_C_SOURCE 199309L
 # include <signal.h>
 // extern int	g_signal = 0;
 
-int     get_signal();
-void    ctl_c_handler(int sig);
-void    ctl_d_handler(int sig);
+int		get_signal(void);
+void	ctl_c_handler(int sig);
+void	ctl_d_handler(int sig);
 // int ctl_backslash_handler(int sig);
 
 
--- srcs/get_signal.c
+++ srcs/get_signal.c
@@ -6,11 +6,12 @@
 /*   By: jbaumfal <jbaumfal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:57:23 by jbaumfal          #+#    #+#             */
-/*   Updated: 2024/11/29 16:33:09 by jbaumfal         ###   ########.fr       */
+/*   Updated: 2024/11/29 18:54:17 by jbaumfal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
 
 int     get_signal();
 void    ctl_c_handler(int sig);
@@ -20,8 +21,16 @@ void    ctl_d_handler(int sig);
 
 int get_signal()
 {
-    struct sigaction *ctl_c;
+    struct sigaction	sa;
 
+	sa.sa_handler = ctl_c_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+	{
+        perror("sigaction");
+        return (1);
+	}
     // signal(SIGINT, ctl_c_handler);
     // signal(SIGQUIT, ctl_d_handler);
     // signal(SIGINT, ctl_c_handler);
@@ -38,7 +47,7 @@ void  ctl_c_handler(int sig)
 void  ctl_d_handler(int sig)
 {
     (void)sig;
-    ft_printf("Received Signal + C\n");
+    ft_printf("Received Signal + D\n");
 }
 
 // int  ctl_backslash_handler(int sig)
